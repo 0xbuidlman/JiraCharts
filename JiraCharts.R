f50f7632672e23b06fa2a66974c32bf917286df3
Jawad Habib <mjawadhabib@gmail.com>
2015-11-24 09:59:16 -0800

    updated code to GET issues from sprints; added code to GET issue work estimates;

--- JiraCharts.R
+++ JiraCharts.R
@@ -1,26 +1,23 @@
+# Libraries ---------------------------------------------------------------
+
+
 setwd("C:/Users/jhabib/Documents/R/JiraCharts")
 library(httr)
 library(jsonlite)
 library(plyr)
 library(reshape2)
+library(data.table)
 
-rv <- "https://aunr-jira-01.ali.local/rest/greenhopper/latest/rapidviews/list"
-rapid_views <- GET(rv, authenticate("jhabib", "November@2015"))
 
-sb <- "https://aunr-jira-01.ali.local/rest/greenhopper/latest/sprintquery/113/?includeHistoricSprints=true&includeFutureSprints=true"
-sprints <- GET(sb)
+# Helpers -----------------------------------------------------------------
 
-board_sprints <- content(sprints, as = "text")
-sp_list <- fromJSON(board_sprints)
-sp_df <- as.data.frame(sp_list[1])
 
-write.csv(sp_df, file = "ngp_sprints.csv")
+json_list_auth <- function(json_url, username, password){
+  jd <- GET(json_url, authenticate(username, password))
+  jd_content <- content(jd, as = "text")
+  return(fromJSON(jd_content))
+}
 
-sp <- "https://aunr-jira-01.ali.local/rest/greenhopper/latest/rapid/charts/sprintreport?rapidViewId=113&sprintId=1326"
-
-sprint_doc <- GET(sp)
-sprint_details <- content(sprint_doc, as = "text")
-sprint_details_list <- fromJSON(sprint_details)
 
 json_list <- function(json_url){
   jd <- GET(json_url)
@@ -28,60 +25,86 @@ json_list <- function(json_url){
   return(fromJSON(jd_content))
 }
 
-completed_issues <- list()
-incompleted_issues <- list()
-punted_issues <- list()
-for(sprint_id in sp_df$sprints.id){
-  sp_url <- paste("https://aunr-jira-01.ali.local/rest/greenhopper/latest/rapid/charts/sprintreport?rapidViewId=113&sprintId=", sprint_id, sep="")
+
+# Get Sprints from Board --------------------------------------------------
+
+
+board_url <- "https://aunr-jira-01.ali.local/rest/greenhopper/latest/sprintquery/113/?includeHistoricSprints=true&includeFutureSprints=true"
+board <- json_list_auth(board_url, "jhabib", "November@2015")
+board_sprints <- board$sprints$id
+# write.csv(board_sprints, file = "ngp_sprints.csv", row.names = FALSE)
+
+
+# Get Issues from Sprints -------------------------------------------------
+
+
+sprint_issues <- data.table()
+completed <- data.table()
+incompleted <- data.table()
+punted <- data.table()
+for(sprint_id in board_sprints){
+  sp_url <- paste("https://aunr-jira-01.ali.local/rest/greenhopper/latest/rapid/charts/sprintreport?rapidViewId=113&sprintId=", 
+                  sprint_id, 
+                  sep="")
   sp_list <- json_list(sp_url)
-  for(ci in sp_list$contents$completedIssues$key){
-    temp <- list(sprint_id, ci)
-    completed_issues <- rbind(completed_issues, temp)
-  }
-  for(ii in sp_list$contents$incompletedIssues$key){
-    temp <- list(sprint_id, ii)
-    incompleted_issues <- rbind(incompleted_issues, temp)
-  }
-  for(pi in sp_list$contents$puntedIssues$key){
-    temp <- list(sprint_id, pi)
-    punted_issues <- rbind(punted_issues, temp)
-  }
-}
-
-
-# ti_url <- "https://aunr-jira-01.ali.local/rest/api/latest/issue/CTOB-97"
-# ti_doc <- GET(ti_url)
-# ti_details <- content(ti_doc, as = "text")
-# ti_details_list <- fromJSON(ti_details)
-
-# df_list <- list(completed_issues, incompleted_issues, punted_issues)
-# df_list <- lapply(df_list, function(x){ as.data.frame(x); x})
-# df_list <- lapply(df_list, 'rownames<-', NULL)
-
-ci_df <- as.data.frame(completed_issues)
-row.names(ci_df) <- NULL
-names(ci_df) <- c("sprint_id", "completed_key")
-ci_df <- data.frame(lapply(ci_df, as.character), stringsAsFactors=FALSE)
-write.csv(ci_df, file = "completed_issues_df.csv", row.names = FALSE)
-
-ii_df <- as.data.frame(incompleted_issues)
-row.names(ii_df) <- NULL
-names(ii_df) <- c("sprint_id", "incompleted_key")
-ii_df <- data.frame(lapply(ii_df, as.character), stringsAsFactors=FALSE)
-write.csv(ii_df, file = "incompleted_issues_df.csv", row.names = FALSE)
-
-pi_df <- as.data.frame(punted_issues)
-row.names(pi_df) <- NULL
-names(pi_df) <- c("sprint_id", "punted_key")
-pi_df <- data.frame(lapply(pi_df, as.character), stringsAsFactors=FALSE)
-write.csv(pi_df, file = "punted_issues_df.csv", row.names = FALSE)
-
-df_list <- list(ci_df, ii_df, pi_df)
-df_merged <- Reduce(function(...) merge(..., all=T), df_list)
-df_merged_new <- melt(df_merged, id.vars = "sprint_id")
-df_merged_new[duplicated(df_merged_new[setdiff(names(df_merged_new), "variable")]), "value"] <- NA
-df_merged_new <- na.omit(df_merged_new)
-
-for(issue in ci_df$key){
   
+  if(!(class(sp_list$contents$completedIssues$key)=="NULL"))
+    completed <- data.table(sprint_id, "completed", sp_list$contents$completedIssues$key)
+  
+  if(!(class(sp_list$contents$incompletedIssues$key)=="NULL"))
+    incompleted <- data.table(sprint_id, "incompleted", sp_list$contents$incompletedIssues$key)
+  
+  if(!(class(sp_list$contents$puntedIssues$key)=="NULL"))
+    punted <- data.table(sprint_id, "punted", sp_list$contents$puntedIssues$key)
+  
+  this_list <- list(sprint_issues, completed, incompleted, punted)
+  sprint_issues <- rbindlist(this_list)
 }
+setnames(sprint_issues, c("sprintid", "issuestatus", "issuekey"))
+write.csv(sprint_issues, file = "sprint_issues.csv", row.names = FALSE)
+
+
+# Get Issue Details ------------------------------------------------------
+
+# JIRA current allows maxResults=1000
+# However, we break our issue list into chunks of 200 issues each
+issue_work_estimates <- data.table()
+
+issue_keys <- unique(sprint_issues$issuekey)
+issue_key_chunks <- split(issue_keys, ceiling(seq_along(issue_keys)/200))
+
+for(i in seq(length(issue_key_chunks))){
+  
+  key_vec <- do.call("paste", c(issue_key_chunks[i], collapse=","))
+  query_url <- paste("https://aunr-jira-01.ali.local/rest/api/latest/search?jql=issuekey%20in%20(", key_vec, ")&maxResults=200&fields%20in%20(id,key,aggregatetimeoriginalestimate,aggregatetimespent,aggregatetimeestimate)", sep = "")
+  issue_details <- json_list(query_url)
+  
+  Sys.sleep(2)
+  
+  this_chunk_data <- data.table(issue_details$issues$key, 
+                                issue_details$issues$fields$aggregatetimeoriginalestimate, 
+                                issue_details$issues$fields$aggregatetimespent, 
+                                issue_details$issues$fields$aggregatetimeestimate)
+  
+  # write.csv(this_chunk_data, paste("chunk", i, ".csv", sep = ""), row.names = FALSE, col.names = FALSE)
+  
+  temp_list <- list(issue_work_estimates, this_chunk_data)
+  issue_work_estimates <- rbindlist(temp_list)
+}
+setnames(issue_work_estimates, c("issuekey", "originalestimate", "timespent", "currentestimate"))
+write.csv(issue_work_estimates, file = "issue_work_estimates.csv", row.names = FALSE)
+
+# Combine Sprint Issues and Issue Work Data -------------------------------
+
+setkey(sprint_issues)
+setkey(issue_work_estimates)
+
+sprint_issue_combined <- merge(sprint_issues, issue_work_estimates, all = TRUE, by = "issuekey")
+write.csv(sprint_issue_combined, file = "sprint_issue_combined.csv", row.names = FALSE)
+
+# Tests -------------------------------------------------------------------
+
+# test_sp_url <- "https://aunr-jira-01.ali.local/rest/greenhopper/latest/rapid/charts/sprintreport?rapidViewId=113&sprintId=1326"
+# test_sp_res <- GET(test_sp_url)
+# test_sp_text <- content(test_sp_res, as = "text")
+# test_sp_list <- fromJSON(test_sp_text)
